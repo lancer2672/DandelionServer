a51283039a7f1af8ba7b44f66c3ab701b1898b55
lancer <jbkhanhtran@gmail.com>
2022-11-07 21:59:44 +0700

    update app.js

--- models/posts.js
+++ models/posts.js
@@ -29,6 +29,27 @@ const PostSchema = new Schema(
       type: Number,
       default: 0,
     },
+    likes: [
+      {
+        creatorName: String,
+        userId: {
+          type: Schema.Types.ObjectId,
+          ref: "users",
+        },
+        createdAt: String,
+      },
+    ],
+    comments: [
+      {
+        creatorName: String,
+        userId: {
+          type: Schema.Types.ObjectId,
+          ref: "users",
+        },
+        content: String,
+        createdAt: String,
+      },
+    ],
   },
   { timestamps: true }
 );
