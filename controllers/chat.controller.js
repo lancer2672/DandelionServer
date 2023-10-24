5ee5caef2e00d16004b2febc7aa0c69357ea7947
lancer2672 <jbkhanhtran@gmail.com>
2023-10-24 07:47:22 +0700

    reformat code: implement class SuccessResponse

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,5 +1,4 @@
 const { default: mongoose } = require("mongoose");
-const ObjectId = require("mongoose").Types.ObjectId;
 const Channel = require("../models/channel");
 const User = require("../models/user");
 const { validationResult } = require("express-validator");
