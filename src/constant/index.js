a7d12a60c1926a93bb364399a23cd1022161dc40
lancer2672 <jbkhanhtran@gmail.com>
2024-03-12 00:18:12 +0700

    add Redis constants

--- src/constant/index.js
+++ src/constant/index.js
@@ -5,27 +5,29 @@ const HEADER = {
   REFRESH_TOKEN: "x-rtoken-id",
 };
 
-const MessageType = {
+const MESSAGE_TYPE = {
   TEXT: "text",
   IMAGE: "image",
   VIDEO: "video",
   CALL_HISTORY: "callHistory",
 };
 
-const FriendRequestStatus = {
+const FRIEND_REQUEST_STATUS = {
   ACCEPT: "accept",
   DECLINE: "decline",
   PENDING: "pending",
 };
-const RedisConnectStatus = {
+const REDIS_CONNECTION_STATUS = {
   CONNECT: "connect",
   END: "end",
   RECONNECT: "reconnect",
   ERROR: "error",
 };
+const REDIS_CONNECTION_TIMEOUT = 10000;
 module.exports = {
   HEADER,
-  MessageType,
-  FriendRequestStatus,
-  RedisConnectStatus,
+  REDIS_CONNECTION_TIMEOUT,
+  MESSAGE_TYPE,
+  FRIEND_REQUEST_STATUS,
+  REDIS_CONNECTION_STATUS,
 };
